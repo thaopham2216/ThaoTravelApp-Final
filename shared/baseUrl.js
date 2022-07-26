88ee6d3a3463a71388eee1b7f1371713fae5b273
Thao Pham <64708940+thaopham2216@users.noreply.github.com>
2022-07-26 14:43:56 -0400

    Update baseUrl.js

--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,18 +1,6 @@
 // House
-//export const baseUrl = 'http://192.168.0.218:3001/';
+//export const baseUrl = 'http://111.111.0.111:3001/';
 
 // house extension wifi 
-export const baseUrl = 'http://192.168.0.184:3001/';
+export const baseUrl = 'http:/111.111.0.111:3001/';
 
-
-// PJ house
-// export const baseUrl = 'http://192.168.4.38:3001/';
-
-// Amy house
-//export const baseUrl = 'http://192.168.1.82:3001/';
-
-// change this IPv4 address whenever change internet
-//export const baseUrl = 'http://10.201.8.184:3001/';
-
-// start json server
-// $ json-server -H 0.0.0.0 --watch db.json -p 3001 -d 2000
